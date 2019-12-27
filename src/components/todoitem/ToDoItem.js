50f579d93142274c92ae986ce112795a57a66cf7
Sheetal Sharma <codemyid@gmail.com>
2019-12-27 19:14:29 +0530

    button component added

--- src/components/todoitem/ToDoItem.js
+++ src/components/todoitem/ToDoItem.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Button from '../buttons/Button';
 
 const ToDoItem = () => {
     return (
-        <li> Item 1 <button> Remove</button></li>
+        <li> Item 1 <Button text="Remove" handleClick={ () => remove()} /> </li>
     );
 }
+
+const remove = () => {
+    alert('remove clicked')
+  }
 
 export default ToDoItem;
